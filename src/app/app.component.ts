2dbc1bd58920244dc273b4a9a7521b84636cd13c
Elie Bertha <elie.bertha@transfer-to.com>
2017-03-12 20:11:56 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+  			    './../css/bootstrap.min.css',
+                '../css/main.css',
+                '../css/font-awesome.min.css',
+                '../css/lineabasic.css',
+                '../css/animate.min.css',
+                '../css/slick.css',
+                '../css/magnific-popup.css',
+             ]
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
